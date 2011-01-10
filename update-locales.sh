e39f27e00be098828d55305e1b54c573338f0b19
Christopher Finke <cfinke@gmail.com>
2011-01-10 00:34:56 +0000

    Removing build script

--- update-locales.sh
+++
@@ -1,9 +0,0 @@
-mkdir .tmp
-curl -o ".locales.zip" "http://interpr.it/api/download?extension_id=3"
-mv .locales.zip .tmp/
-cd .tmp/
-unzip .locales.zip
-rm .locales.zip
-cp -r * ../tubestop-chrome/_locales/
-cd ..
-rm -rf .tmp/
