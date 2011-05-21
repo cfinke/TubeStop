5c5c865ca483be525c2f87301156651533741a25
Christopher Finke <cfinke@gmail.com>
2011-05-21 21:23:24 +0000

    Updated for compatibility with YouTube HTML changes

--- tubestop-chrome/tubestop.js
+++ tubestop-chrome/tubestop.js
@@ -17,38 +17,26 @@ var TUBESTOP = {
 		var playerDiv = page.getElementById("watch-player");
 		
 		if (playerDiv) {
-			if (page.body.innerHTML.indexOf("'EMBEDDING_DISABLED':") != -1) {
-				var script = playerDiv.getElementsByTagName("script")[0];
-				var code = script.innerHTML.match(/swfHTML[^\n]+"\s+:\s+"(<embed.*?)<noembed/i)[1];
-				
-				playerDiv.innerHTML = "";
-				
-				code = code.replace(/([^\\])"\s*\+\s*"/g, "$1");
-				code = code.replace(/\\/g, "");
-				
-				var a = page.createElement("a");
-				a.setAttribute("href", "javascript:void(0);");
-				a.addEventListener("click", function () { this.parentNode.innerHTML = code; }, false);
-				a.addEventListener("mouseover", function () { this.style.backgroundColor = "#eee"; }, false);
-				a.addEventListener("mouseout", function () { this.style.backgroundColor = "#fff"; }, false);
-				a.style.display = "block";
-				a.style.paddingTop = "170px";
-				a.style.paddingBottom = "170px";
-				a.style.width = "640px";
-				a.style.fontSize = "12pt";
-				a.style.textAlign = "center";
-				a.innerHTML = chrome.i18n.getMessage("clickToPlay");
-				
-				playerDiv.appendChild(a);
-			}
-			else {
-				var url = page.location.href;
-				var videoId = url.match(/v=([^&]+)+(?:&.*)?$/)[1];
-				
-				var embedCode = '<object width="640" height="385"><param name="movie" value="http://www.youtube.com/v/'+videoId+'&hl=en_US&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/'+videoId+'&hl=en_US&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="385"></embed></object>';
-		
-				playerDiv.innerHTML = embedCode;
-			}
+			var code = playerDiv.innerHTML;
+			
+			playerDiv.innerHTML = "";
+			
+			var a = page.createElement("a");
+			a.setAttribute("href", "javascript:void(0);");
+			a.addEventListener("click", function () { this.parentNode.innerHTML = code; }, false);
+			a.addEventListener("mouseover", function () { this.style.backgroundColor = "#eee"; }, false);
+			a.addEventListener("mouseout", function () { this.style.backgroundColor = "#fff"; }, false);
+			a.style.display = "block";
+			a.style.paddingTop = "180px";
+			a.style.paddingBottom = "180px";
+			a.style.width = "640px";
+			a.style.fontSize = "12pt";
+			a.style.textAlign = "center";
+			a.style.border = "1px dotted gray";
+			a.style.background = "#fff";
+			a.innerHTML = chrome.i18n.getMessage("clickToPlay");
+			
+			playerDiv.appendChild(a);
 		}
 	},
 	
